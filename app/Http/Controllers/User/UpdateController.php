4b0158e8e89aa6af96714965188b7843aac280ac
Nic <nic@nybbl.io>
2019-11-20 18:41:35 +0000

    Updated the UpdateController.

--- app/Http/Controllers/User/UpdateController.php
+++ app/Http/Controllers/User/UpdateController.php
@@ -5,12 +5,25 @@ declare(strict_types=1);
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\UserResource;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UpdateController extends Controller
 {
     public function __invoke(Request $request)
     {
+        /** @var User $user */
+        $user = $request->user();
 
+        $this->validate($request, [
+
+        ]);
+
+        $user->update([
+
+        ]);
+
+        return new UserResource($user);
     }
 }
